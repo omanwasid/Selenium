5efe310a31db41682f759fbca3638c2402532032
Muhammad Al Mahmud Wasid <oman46@gmail.com>
2023-12-01 10:06:46 +0100

    tesntNG

--- src/test/java/com/testNG/SauceDemoTest.java
+++ src/test/java/com/testNG/SauceDemoTest.java
@@ -22,21 +22,11 @@ public class SauceDemoTest {
 
         Assert.assertEquals(driver.getTitle(), "Swag Labs");
 
-
-    }
-    @Test
-    public  void verifyTitle2ShouldSucceed() throws InterruptedException {
-        WebDriverManager.firefoxdriver().setup();
-        WebDriver driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-
-        driver.get("https://demoqa.com/");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
-        Assert.assertEquals(driver.getTitle(), "DEMOQA");
+        driver.quit();
 
 
     }
+
     @Test
     public  void verifyLoginShouldSucceed() throws InterruptedException {
         WebDriverManager.firefoxdriver().setup();
@@ -45,12 +35,34 @@ public class SauceDemoTest {
 
         driver.get("https://www.saucedemo.com/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
         driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.cssSelector("#login-button")).click();
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
 
-
+    driver.quit();
     }
+    @Test
+    public  void verifyProductPageShouldSucceed() throws InterruptedException {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+        driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("#login-button")).click();
+        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
+        driver.findElement(By.cssSelector(".shopping_cart_link")).click();
+        //Assert.assertEquals(driver.findElements(By.linkText("Checkout: Your Information")),"Checkout: Your Information");
+
+        Assert.assertEquals(driver.getCurrentUrl(),"https://www.saucedemo.com/cart.html");
+
+    driver.quit();
+    }
+
 }
+
